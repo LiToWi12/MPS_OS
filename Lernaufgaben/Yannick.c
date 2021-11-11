f78bdf00941f63f1a818c511a87ad683cb4092dd
Y4nn1ng <77332919+Y4nn1ng@users.noreply.github.com>
2021-11-11 11:12:30 +0100

    Update Yannick.c

--- Lernaufgaben/Yannick.c
+++ Lernaufgaben/Yannick.c
@@ -9,7 +9,25 @@ void main() {
     days = days % 365;
     printf("Wochen: %d\n", (days / 7));
     days = days % 7;
-    printf("Tage: %d\n", days);
-    
+    printf("Tage: %d\n", days);    
 
+}
+
+
+
+// Aufgabe 1.1 modifiziert
+
+#include <stdio.h>
+
+char name[30];
+int age;
+
+void main() {
+    
+    printf("Dein Name ist?\n");
+    scanf("%s", &name);
+    printf("Dein Alter ist?\n");
+    scanf("%d", &age);
+    printf("Du bist %s und bist %03d Jahre alt!", name, age);
+    
 }

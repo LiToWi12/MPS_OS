73f50f8163d3f8e91c8bf00b91cae8542772b045
moritz-kra <93642616+moritz-kra@users.noreply.github.com>
2021-11-17 08:47:57 +0100

    Update Moritz.c

--- Lernaufgaben/Moritz.c
+++ Lernaufgaben/Moritz.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int a =12;
+    int *b = NULL;
+    int c =13;
+    int *d = NULL;
+    int e = NULL;
+    
+    
+    printf("a-Wert\t");
+    scanf("%d" , &a);
+    printf("c-Wert\t");
+    scanf("%d" , &c);
+    
+    b =  &a;
+    d =  &c;
+    e=*b+*d;
+    
+    printf("\na-Wert: %d\n" , a);
+    printf("c-Wert: %d\n" , c);
+    printf("e-Wert: %d\n" , e);
+}
 

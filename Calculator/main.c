40ea1a82185141d54dae3b397e91c367035d6514
Y4nn1ng <77332919+Y4nn1ng@users.noreply.github.com>
2021-11-18 11:15:23 +0100

    Update main.c

--- Calculator/main.c
+++ Calculator/main.c
@@ -1,13 +1,41 @@
 #include <stdio.h>
-#define _USE_MATH_DEFINES
-#include <math.h>
 
-float radius;
+float number[100];
+char op[100];
 
-void main() {
-    
-    int a;
-    scanf("%d ", &a);
-    scanf
+void input() {
+
+    int i = 0;
+    int boolean = 0;
+    float numberTmp;
+    float opTmp;
+
+    while (boolean == 0) {
+
+        printf("eins");
+        scanf("%f", &numberTmp);
+        printf("zwei");
+        scanf("%c", &opTmp);
+
+        number[i] = numberTmp;
+        op[i] = opTmp;
+
+        if (opTmp == '=') {
+            boolean = 1;
+        }
+
+        i++;
+
+    }
 
 }
+
+
+void main() {
+
+    input();
+    
+    printf("Hi!");
+
+}
+
